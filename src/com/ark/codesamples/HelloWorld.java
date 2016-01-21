38705fc1c38ab156dc75c716d90ff48f5264ff37
amitrk3 <amit.rk3@gmail.com>
2016-01-21 21:48:47 +0530

    Update HelloWorld.java

--- src/com/ark/codesamples/HelloWorld.java
+++ src/com/ark/codesamples/HelloWorld.java
@@ -1,1 +1,9 @@
+package com.ark.codesamples;
 
+public Class HelloWorld{
+  
+  public static void main(String args[]){
+    System.out.print("Hi there,\n you expected hello world I know");
+  }
+  
+}
